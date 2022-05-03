211bd68abef7859f10510e96d357930831dea634
Raylin51 <raylzj51@gmail.com>
2022-05-04 00:38:40 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,92 @@
+#/bin/bash
+
+evm='https://github.com/KYVENetwork/evm/releases/download/v1.0.3/evm-linux.zip'
+bitcion='https://github.com/kyve-org/bitcoin/releases/download/v0.0.0/kyve-bitcoin-linux.zip'
+solana='https://github.com/kyve-org/solana/releases/download/v0.0.0/kyve-solana-linux.zip'
+zilliqa='https://github.com/kyve-org/zilliqa/releases/download/v0.0.0/kyve-zilliqa-linux.zip'
+
+function checkInt() {
+  expr $1 + 0&>/dev/null
+  [ $? -ne 0 ] && { echo "$2 must be integer."; return 1; }
+  return 0
+}
+function checkMenmonic() {
+  [ $(echo $#) -ne 12 -a $(echo $#) -ne 24 ] && { echo "Menmonic must be 12 or 24 words."; return 1; }
+  return 0
+}
+function checkPath() {
+  [ ! -f "$1" ] && { echo "It must be a file path."; return 1; }
+  return 0
+}
+
+until [[ $? -eq 0 ]] && [[ -n "$pool" ]]
+do
+  read -p 'Pool ID: ' pool
+  checkInt $pool echo "Pool ID"
+done
+until [ $? -eq 0 ] && [[ -n "$mnemonic" ]]
+do
+  read -p 'Mnemonic: ' mnemonic
+  checkMenmonic $mnemonic
+done
+until [ $? -eq 0 ] && [[ -n "$initialStake" ]]
+do
+  read -p 'InitialStake: ' initialStake
+  checkInt $initialStake echo "Initial stake"
+done
+
+read -p 'Ar wallet json(If you saved json file on this device, just press Enter): ' arjson
+if [ -z "$arjson" ]
+then
+  until [[ $? -eq 0 ]] && [[ -n "$arpath" ]]
+  do
+    read -e -p 'Ar wallet path: ' arpath
+    checkPath $arpath
+  done
+else
+  echo $arjson > $HOME/ar.json
+  arpath="$HOME/ar.json"
+fi
+
+if [ $pool -eq 0 -o $pool -eq 1 ]
+then
+  url=$evm
+elif [ $pool -eq 3 ]
+then
+  url=$bitcoin
+elif [ $pool -eq 4 ]
+then
+  url=$solana
+elif [ $pool -eq 5 ]
+then
+  url=$zilliqa
+else
+  url=$evm
+fi
+
+cd $HOME
+wget $url
+filename=${url##*/}
+ziplog=$(unzip -o -d /usr/local/bin $filename | grep '/usr/local/bin/')
+binary=${ziplog##*/}
+chmod a+rx /usr/local/bin/$binary
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/kyve.service
+[Unit]
+Description=Kyve Validator
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$binary --poolId $pool --mnemonic $mnemonic --initialStake $initialStake --keyfile $arpath --network korellia
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable kyve
+systemctl daemon-reload
+systemctl start kyve
